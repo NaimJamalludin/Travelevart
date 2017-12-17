2042f8ad1f2abbe83b71fdb68d1fa40d422edd57
Naim Jamalludin <naim.jamalludin@gmail.com>
2017-12-17 15:01:17 +0800

    Redirect login and logout to main page

--- travel/travel/settings.py
+++ travel/travel/settings.py
@@ -126,6 +126,10 @@ STATICFILES_DIRS = [
       os.path.join(BASE_DIR,"static"),
 ]
 
+LOGIN_REDIRECT_URL = '/'
+
+LOGOUT_REDIRECT_URL = '/'
+
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static_cdn")
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR),"media_cdn")
