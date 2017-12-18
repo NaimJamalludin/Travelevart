43aa3a4168ce8d399a6673fbe43be8d95af65d63
Naim Jamalludin <naim.jamalludin@gmail.com>
2017-12-18 18:54:52 +0800

    Added Article API support

--- travel/article/urls.py
+++ travel/article/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 app_name = 'article'
@@ -10,4 +11,9 @@ urlpatterns = [
 
 	# Method 2
 	url(r'^(?P<pk>[0-9]+)', views.DetailView.as_view(), name = 'detail'),
+
+	# api links for album /albumapi/
+    url(r'^articleapi/', views.ArticleList.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
