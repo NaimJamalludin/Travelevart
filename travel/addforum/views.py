fa8f3b0d7933520797c59a570633e87050593552
BENGAL TIGER <bdzebel@gmail.com>
2017-12-19 09:46:44 +0800

    add forum function added

---
+++ travel/addforum/views.py
@@ -0,0 +1,28 @@
+from django.views.generic import TemplateView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .forms import ForumForm
+from forum.models import Forum
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class AddForumFormView(LoginRequiredMixin, TemplateView):
+	login_url = "login/"
+	template_name = 'addforum/addforum.html'
+
+	def get(self, request):
+		form = ForumForm()
+		forums = Forum.objects.all()
+		args = {'form': form, 'forums': forums}
+		return render(request, self.template_name, args)
+
+	def post(self, request):
+			form = ForumForm(request.POST)
+			if form.is_valid():
+				forum = form.save(commit = False)
+				forum_title = form.cleaned_data['forum_title']
+				forum_items = form.cleaned_data['forum_items']
+				forum.save()
+				args = {'form': form, 'forum_title': forum_title, 'forum_items': forum_items}
+				return HttpResponseRedirect(reverse('forum:forumindex'))
--- travel/forum/models.py
+++ travel/forum/models.py
@@ -3,11 +3,15 @@ from django.utils import timezone
 #from django.core.urlresolvers import reverse
 from django.urls import resolve, reverse
 from django.contrib.auth.models import User
+from autoslug import AutoSlugField
+from django.template.defaultfilters import slugify
+import datetime
 
 # Create your models here.
 class ListForum(models.Model):
 	itemlist = models.CharField(max_length = 1500)
-	slug = models.SlugField(max_length=250)
+	slug = AutoSlugField(populate_from = 'itemlist',unique=True)
+
 
 	def get_absolute_url(self):
 		return reverse('forum:detail',args=[self.slug])
@@ -18,8 +22,9 @@ class ListForum(models.Model):
 
 class Forum(models.Model):
     forum_title = models.CharField(max_length = 250)
-    content = models.TextField()
-    images = models.ImageField(blank=True)
+    #slug = AutoSlugField(populate_from = 'forum_title',unique=True)
+    #content = models.TextField()
+    #images = models.ImageField(blank=True)
     forum_items = models.ManyToManyField(ListForum)
     def __str__(self):
         return self.forum_title
